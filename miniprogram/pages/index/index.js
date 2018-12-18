de9e08edc20504b3a7cbbf176496a6f996f4a2ed
vanenea <576700973@qq.com>
2018-12-18 17:34:13 +0800

    18.December.2018

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -26,6 +26,34 @@ Page({
         title: "测试数据测试数据",
         price: "78.5"
       }
-    ]
+    ],
+    type: "home",
+    homeImage: "../../../images/like_a.png",
+    categoryImage: "../../../images/category.png",
+    cartImage: "../../../images/cart.png",
+    meImage: "../../../images/me.png"
+  },
+  footerTap(event){
+    this.type = event.currentTarget.dataset.type;
+    if(this.type=="home"){
+      this.setData({
+        type : this.type,
+        homeImage : "../../../images/like_a.png",
+        categoryImage : "../../../images/category.png"
+      });
+    } else if (event.currentTarget.dataset.type == "category"){
+      this.setData({
+        type: this.type,
+        homeImage : "../../../images/like.png",
+        categoryImage : "../../../images/category_a.png"
+
+      })
+    }
+  },
+  detail(event){
+    wx.redirectTo({
+      url: '../detail/detail'
+
+    })
   }
 })
