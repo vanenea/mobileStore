48477a876cd2fa916b9623f63c0d7f28a32230d1
vanenea <576700973@qq.com>
2018-12-25 17:34:12 +0800

    25.Demcember.2018

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -139,5 +139,52 @@ Page({
       current: event.currentTarget.dataset.imgsrc
 
     })
+  },
+  /**
+   * 显示遮罩层
+   */
+  showModal: function () {
+    
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+      showModalStatus: true
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export()
+      })
+    }.bind(this), 200)
+  },
+  /**
+   * 隐藏遮罩层
+   */
+  hideModal: function () {
+    var animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: "linear",
+      delay: 0
+    })
+    this.animation = animation
+    animation.translateY(300).step()
+    this.setData({
+      animationData: animation.export(),
+    })
+    setTimeout(function () {
+      animation.translateY(0).step()
+      this.setData({
+        animationData: animation.export(),
+        showModalStatus: false
+      })
+    }.bind(this), 200)
   }
+
+
 })
